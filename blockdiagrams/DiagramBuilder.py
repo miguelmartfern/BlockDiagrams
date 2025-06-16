82273bb28d9934116e770a3757d59bbbc3c903af
Miguel Á. Martín-Fernández <migmar@tel.uva.es>
2025-06-16 18:21:46 +0200

    Changes: * GNU license header in SignalPlotter.py

--- blockdiagrams/DiagramBuilder.py
+++ blockdiagrams/DiagramBuilder.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+# DiagramBuilder.py
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Rectangle, FancyArrow
